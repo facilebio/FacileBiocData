fc6b94c04d944e365822ef8b5aec875e67e6747f
lianos <lianoglou@dnli.com>
2020-08-27 22:32:37 -0700

    Pin GHA setup-r action to commit prior v1
    Need to figure out how to ensure that we can skip the (re)-installation
    of R when running the GHA in a container.

--- R/facileapi-base.R
+++ R/facileapi-base.R
@@ -1,5 +1,15 @@
 #' @noRd
 #' @export
+name.FacileBiocDataStore <- function(x, ...) {
+  name <- ifacile(x)[["name"]]
+  if (is.null(name)) {
+    name <- paste(colnames(x), collapse = ":")
+  }
+  name
+}
+
+#' @noRd
+#' @export
 organism.FacileBiocDataStore <- function(x, ...) {
   out <- ifacile(x)[["organism"]]
   if (is.null(out)) out <- "unknown"
